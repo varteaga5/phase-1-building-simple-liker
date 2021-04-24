a20e65b18c1a64b323e89c94ff41d0b821c228bf
vincent <vincent.arteaga@gmail.com>
2021-04-23 21:08:01 -0700

    commit tree c0ac8c852ee7effcc7a5e6348a531a938727cc00

--- main.js
+++ main.js
@@ -3,7 +3,26 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// user clicks on empty heart
+const heart = document.querySelectorAll('.like-glyph');
+heart.forEach(element => element.addEventListener('click', hClick))
+function hClick(e) {
+  // Invoke mimicServerCall to simulate making a server request
+  // When the "server" returns a failure status:
+  if(!mimicServerCall()){
+    // Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+    // Display the error modal by removing the .hidden class
+    // Display the server error message in the modal
+    // Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+  } else {
+    // When the "server" returns a success status:
+    // Change the heart to a full heart
+    // Add the .activated-heart class to make the heart appear red
 
+  }
+
+  console.log(e.target.parentNode)
+}
 
 
 
@@ -11,9 +30,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
